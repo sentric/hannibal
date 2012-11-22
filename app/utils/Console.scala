96b48320a5e2c4ee60eadbe45a7ebef6fcd60821
nils.kuebler <nils.kuebler@memonews.com>
2012-11-22 18:41:26 +0100

    add Console.stopApp()

--- app/utils/Console.scala
+++ app/utils/Console.scala
@@ -6,4 +6,5 @@ package utils
 
 object Console {
    def startApp() = play.api.Play.start(new play.api.Application(new java.io.File("."), classOf[play.core.StaticApplication].getClassLoader, None, play.api.Mode.Test))
+   def stopApp() = play.api.Play.stop()
 }
