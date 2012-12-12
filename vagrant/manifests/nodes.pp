d4221cd729de72039bae8cc320aeec6fab178eea
nils.kuebler <nils.kuebler@memonews.com>
2012-12-12 17:57:03 +0100

    the test-data generator is now started together with the machine

--- vagrant/manifests/nodes.pp
+++ vagrant/manifests/nodes.pp
@@ -1,3 +1,14 @@
 node dev {
   include cloudera::hbase::standalone
+
+  file { "/etc/init.d/hbase-testdata-generator":
+    ensure => "link",
+    target => "/vagrant/vagrant/files/hbase-testdata-generator-daemon.sh",
+    require => Service["hadoop-hbase-master"]
+  }
+
+  service { "hbase-testdata-generator":
+    ensure => "running",
+    require => File["/etc/init.d/hbase-testdata-generator"]
+  }
 }
