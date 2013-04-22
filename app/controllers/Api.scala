42eeb7139b8e3148c88f6b99e80ddfff09d7c39f
Nils Kübler <nils.kuebler@ymc.ch>
2013-04-22 16:15:01 +0200

    add /api/dashboard which returns the most important numbers

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -5,6 +5,8 @@ package controllers
 
 import play.api.mvc._
 import play.api.libs.json.Json._
+import utils.MetricUtil
+import models.RegionName
 
 object Api extends Controller {
 
@@ -12,5 +14,26 @@ object Api extends Controller {
     Ok(stringify(toJson(Map(
       "status" -> toJson("OK")
     )))).as("application/json")
+  }
+
+  def dashboard = Action { implicit request =>
+    Ok(stringify(toJson(Map(
+      "compaction_duration" -> compactionDuration
+    )))).as("application/json")
+  }
+
+  private def compactionDuration = {
+    MetricUtil.findLongestCompactionInLastWeek() match {
+      case Some(longestCompaction) =>
+        val regionName = RegionName(longestCompaction._3)
+        toJson(Map(
+          "duration" -> toJson(longestCompaction._1),
+          "timestamp" -> toJson(longestCompaction._2.getTime),
+          "region" -> toJson(longestCompaction._3),
+          "regionName" -> toJson(regionName)
+        ))
+      case None =>
+        toJson("none")
+    }
   }
 }
--- test/controllers/ApiSpec.scala
+++ test/controllers/ApiSpec.scala
@@ -7,6 +7,8 @@ package controllers
 import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
+import models.{RegionName, MetricDef}
+import play.api.libs.json.Json._
 
 class ApiSpec extends Specification  {
   "/api" should {
@@ -16,6 +18,34 @@ class ApiSpec extends Specification  {
           val result = routeAndCall(FakeRequest(GET, "/api/heartbeat"));
           status(result.get) must equalTo(200)
           contentAsString(result.get) must equalTo("{\"status\":\"OK\"}")
+        }
+      }
+    }
+
+    "provide a method: /dashboard" >> {
+      "returning 'none' when there is no longest compaction" >> {
+        running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+          val result = routeAndCall(FakeRequest(GET, "/api/dashboard"));
+          status(result.get) must equalTo(200)
+          contentAsString(result.get) must equalTo("{\"compaction_duration\":\"none\"}")
+        }
+      }
+      "returning the numbers for the compaction compaction" >> {
+        running(FakeApplication(additionalConfiguration = inMemoryDatabase())) {
+          val region = "article,com.redtra,1344630511217.7b6e9618bdac4d4251324e57b3e4084d."
+          val metricDef = MetricDef.findRegionMetricDef(region, "compactions")
+          val anchor = MetricDef.now
+          metricDef.update(1, anchor - 2000)
+          metricDef.update(0, anchor - 1000)
+          val result = routeAndCall(FakeRequest(GET, "/api/dashboard"));
+          status(result.get) must equalTo(200)
+
+          contentAsString(result.get) must equalTo("{\"compaction_duration\":{\"duration\":%d,\"timestamp\":%d,\"region\":\"%s\",\"regionName\":%s}}".format(
+            1000,
+            anchor - 2000,
+            region,
+            stringify(toJson(RegionName(region)))
+          ))
         }
       }
     }
