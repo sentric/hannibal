a4ec39015599e207026d42e1974d5158fe3bac7e
Vitaliy Morarian <vmorarian@gmail.com>
2013-11-09 11:51:42 +0200

    fixes for hbase 0.90

--- hbase/0.90/scala/models/hbase090/HBase090.scala
+++ hbase/0.90/scala/models/hbase090/HBase090.scala
@@ -4,14 +4,14 @@
 
 package models.hbase090
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import models.hbase.{HBase, RegionServer}
 
 class HBase090 extends HBase {
   override def eachRegionServer[T](func: RegionServer => T) = {
     withAdmin { hbaseAdmin =>
       val status = hbaseAdmin.getClusterStatus()
-      val serverInfos = status.getServerInfo()
+      val serverInfos = status.getServerInfo().asScala.toList
       serverInfos.map { serverInfo =>
         func(new RegionServer090(serverInfo))
       }
--- hbase/0.90/scala/models/hbase090/RegionServer090.scala
+++ hbase/0.90/scala/models/hbase090/RegionServer090.scala
@@ -6,16 +6,16 @@ package models.hbase090
 
 import org.apache.hadoop.hbase.HServerLoad.RegionLoad
 import org.apache.hadoop.hbase.HServerInfo
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 import models.hbase.RegionServer
 
 class RegionServer090(val serverInfo: HServerInfo) extends RegionServer {
   override val serverName = serverInfo.getServerName
   override val hostName = serverInfo.getHostname
-  override val port = serverInfo.getServerAddress().getPort
+  override val port = serverInfo.getServerAddress.getPort
   override val infoPort = serverInfo.getInfoPort
   override lazy val load = serverInfo.getLoad
-  override lazy val regionsLoad = load.getRegionsLoad
+  override lazy val regionsLoad = load.getRegionsLoad.asScala
 
   override def equals(that:Any) = {
     that match {
