0ddade76fc5b21178b77dadf41da54310b8dde7e
Amit Anand <aanand59@bloomberg.net>
2016-09-22 14:11:47 -0400

    Add models for HBase 1.1.1

---
+++ hbase/1.1.1/scala/models.hbase111/LogFileParser111.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014 YMC. See LICENSE for details.
+ */
+
+package models.hbase111
+
+import models.LogFile
+import java.util.regex._
+import java.util.Date
+import models.hbase.LogFileParser
+
+class LogFileParser111 extends LogFileParser {
+
+  val COMPACTION = Pattern.compile(
+    """^(.*) INFO (.*).HStore: Completed (.*)compaction of (.*) of (.*\.) into (.*) and took (.*) to execute(.*)$""",
+    Pattern.MULTILINE
+  )
+  val DATE_GROUP = 1
+  val REGION_GROUP = 5
+  val DURATION_GROUP = 7
+
+  override def eachCompaction(logFile: LogFile, functionBlock: (String, Date, Long) => Unit) = {
+    val m = COMPACTION.matcher(logFile.tail())
+    while(m.find()) {
+      val date = m.group(DATE_GROUP)
+      val region = m.group(REGION_GROUP)
+      val duration = m.group(DURATION_GROUP)
+
+      val end = LogFile.dateFormat.parse(date)
+      val durationMsec = parseDuration(duration)
+
+      functionBlock(region, end, durationMsec)
+    }
+  }
+}
---
+++ hbase/1.1.1/scala/models.hbase111/RegionLoad111.scala
@@ -0,0 +1,15 @@
+package models.hbase111
+
+import org.apache.hadoop.hbase.RegionLoad
+
+
+/**
+ * Created by Amit Anand on 09/22/2016.
+ */
+class RegionLoad111(val hbaseRegionLoad:RegionLoad) extends models.hbase.RegionLoad {
+  override val memStoreSizeMB: Float = hbaseRegionLoad.getMemStoreSizeMB
+  override val storeFileSizeMB: Float = hbaseRegionLoad.getStorefileSizeMB
+  override val stores: Int = hbaseRegionLoad.getStores
+  override val storeFiles: Int = hbaseRegionLoad.getStorefiles
+  override val name = hbaseRegionLoad.getName
+}
