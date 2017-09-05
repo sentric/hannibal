d73e02d402f8716ec457842c42ccf508096e7691
Amit Anand <aanand59@bloomberg.net>
2017-09-05 12:27:48 -0400

    Authenticate using keytab(if exists)

--- app/models/hbase/HBase.scala
+++ app/models/hbase/HBase.scala
@@ -7,10 +7,9 @@ package models.hbase
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.client.{Connection, ConnectionFactory, Admin, Table}
 import org.apache.hadoop.hbase.{HBaseConfiguration, HTableDescriptor}
-//import org.apache.hadoop.hbase.util.Bytes
 import org.apache.hadoop.security.UserGroupInformation
 import org.apache.hadoop.hbase.TableName
-
+import play.api.Logger
 
 /**
  * Abstract Factory to access HBase-API. Allows to separate parts that changed between HBase 0.90 and HBase 0.92.
@@ -18,20 +17,36 @@ import org.apache.hadoop.hbase.TableName
  * The concrete implementation is found in hannibal/hbase/[version]/scala
  */
 trait HBase {
+
   val conf:Configuration = HBaseConfiguration.create()
   val AUTH_METHOD = conf.get("hadoop.security.authentication", "SIMPLE")
 
-  if (AUTH_METHOD.toUpperCase == "KERBEROS")
+  Logger.info("Authentication Method : " + AUTH_METHOD)
+
+  if (AUTH_METHOD.toUpperCase == "KERBEROS") {
+
+    val DEFAULT_KT_FILE = "/etc/security/keytabs/hannibal.service.keytab"
+    val DEFAULT_KT_USER = "hannibal"
+    val KT_FILE_KEY = "hannibal.kerberos.keytab"
+    val KT_USER_KEY = "hannibal.kerberos.principal"
+
+    val HANNIBAL_KT_FILE = conf.get(KT_FILE_KEY, DEFAULT_KT_FILE)
+    val HANNIVAL_KT_USER = conf.get(KT_USER_KEY, DEFAULT_KT_USER)
+
+    Logger.info("Hannibal keytab : " + HANNIBAL_KT_FILE)
+    Logger.info("Hannibal User : " + HANNIVAL_KT_USER)
+
     UserGroupInformation.setConfiguration(conf)
 
+    if(new java.io.File(HANNIBAL_KT_FILE).exists())
+      UserGroupInformation.loginUserFromKeytab(HANNIVAL_KT_USER, HANNIBAL_KT_FILE)
+  }
+
   val conn:Connection = ConnectionFactory.createConnection(conf)
-  //val client:Admin = conn.getAdmin();
 
   def eachRegionServer[T](func: RegionServer => T): List[T]
 
   def withHTable[T](tableName: String, func: Table => T): T = {
-    //val conf = HBaseConfiguration.create()
-    //val table = new HTable(conf, Bytes.toBytes(tableName))
     val table:Table = conn.getTable(TableName.valueOf(tableName))
     try {
       func(table)
@@ -56,13 +71,4 @@ trait HBase {
       client.close()
     }
   }
-
-//  def withAdmin[T](func: HBaseAdmin => T): T = {
-//    val client = new HBaseAdmin(conf)
-//    try {
-//      func(client)
-//    } finally {
-//      client.close()
-//    }
-//  }
 }
